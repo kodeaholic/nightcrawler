cbda975e3b88786d8f3ede40cc733c6edde3a123
seta-nathan <nnguyen4@veritone.com>
2021-08-07 12:02:53 +0700

    Add todo, done, failed

--- client/src/app/co-phim/link-le.js
+++ client/src/app/co-phim/link-le.js
@@ -1,5 +1,5 @@
 import React, { Component, useState, useEffect } from "react";
-import { ProgressBar } from "react-bootstrap";
+import { ProgressBar, Pagination } from "react-bootstrap";
 import _ from "lodash";
 import { linkLeService } from "./link-le.service";
 const STATUS = {
@@ -26,22 +26,37 @@ const Table = (props) => {
   const [results, setResults] = useState([]);
   const [totalResults, setTotalResults] = useState(0);
   const [limit, setLimit] = useState(20);
+  const [pagination, setPagination] = useState(<></>);
   useEffect(() => {
     const fetchItems = async () => {
       const res = await linkLeService.getItems(status, page, limit);
       setResults(res.results || []);
       setTotalPage(res.totalPages || 0);
       setTotalResults(res.totalResults || 0);
+    //   if (res.totalPages > 0 && res.totalPages <= 5) {
+    //     let active = res.page;
+    //     let items = [];
+    //     for (let number = 1; number <= res.totalPages; number++) {
+    //       items.push(
+    //         <Pagination.Item key={number} active={number === active}>
+    //           {number}
+    //         </Pagination.Item>
+    //       );
+    //     }
+    //     setPagination(items);
+    //   }
     };
     fetchItems();
-  }, []);
+  }, [page, limit]);
+
   return (
     <div className="card">
       <div className="card-body">
         <h4 className="card-title">{type}</h4>
         <p className="card-description">
           {" "}
-          Total <code>{totalResults}</code> items. Total <code>{totalPage}</code> pages.
+          Total <code>{totalResults}</code> items. Total{" "}
+          <code>{totalPage}</code> pages.
         </p>
         {_.isEmpty(results) && "Empty"}
         {!_.isEmpty(results) && (
@@ -49,57 +64,36 @@ const Table = (props) => {
             <table className="table">
               <thead>
                 <tr>
-                  <th>Profile</th>
-                  <th>VatNo.</th>
-                  <th>Created</th>
+                  <th>No.</th>
+                  <th>Link</th>
+                  <th>Thumb</th>
                   <th>Status</th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Jacob</td>
-                  <td>53275531</td>
-                  <td>12 May 2017</td>
-                  <td>
-                    <label className="badge badge-danger">Pending</label>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Messsy</td>
-                  <td>53275532</td>
-                  <td>15 May 2017</td>
-                  <td>
-                    <label className="badge badge-warning">In progress</label>
-                  </td>
-                </tr>
-                <tr>
-                  <td>John</td>
-                  <td>53275533</td>
-                  <td>14 May 2017</td>
-                  <td>
-                    <label className="badge badge-info">Fixed</label>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Peter</td>
-                  <td>53275534</td>
-                  <td>16 May 2017</td>
-                  <td>
-                    <label className="badge badge-success">Completed</label>
-                  </td>
-                </tr>
-                <tr>
-                  <td>Dave</td>
-                  <td>53275535</td>
-                  <td>20 May 2017</td>
-                  <td>
-                    <label className="badge badge-warning">In progress</label>
-                  </td>
-                </tr>
+                {results.map((item, index) => {
+                  return (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>
+                        <a href={item.link} target="_blank">
+                          {item.link}
+                        </a>
+                      </td>
+                      <td className="py-1">
+                        <img src={item.thumb} alt="Thumb" />
+                      </td>
+                      <td>
+                        <label className="badge badge-info">{type}</label>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
         )}
+        {/* {pagination && <Pagination>{pagination}</Pagination>} */}
       </div>
     </div>
   );
@@ -132,128 +126,7 @@ export class CoPhimLinkLeComponent extends Component {
             <Table type="DONE" />
           </div>
           <div className="col-lg-12 grid-margin stretch-card">
-            <div className="card">
-              <div className="card-body">
-                <h4 className="card-title">FAILED</h4>
-                <p className="card-description">
-                  {" "}
-                  Add className <code>.table-striped</code>
-                </p>
-                <div className="table-responsive">
-                  <table className="table table-striped">
-                    <thead>
-                      <tr>
-                        <th> User </th>
-                        <th> First name </th>
-                        <th> Progress </th>
-                        <th> Amount </th>
-                        <th> Deadline </th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face1.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> Herman Beck </td>
-                        <td>
-                          <ProgressBar variant="success" now={25} />
-                        </td>
-                        <td> $ 77.99 </td>
-                        <td> May 15, 2015 </td>
-                      </tr>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face2.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> Messsy Adam </td>
-                        <td>
-                          <ProgressBar variant="danger" now={75} />
-                        </td>
-                        <td> $245.30 </td>
-                        <td> July 1, 2015 </td>
-                      </tr>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face3.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> John Richards </td>
-                        <td>
-                          <ProgressBar variant="warning" now={90} />
-                        </td>
-                        <td> $138.00 </td>
-                        <td> Apr 12, 2015 </td>
-                      </tr>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face4.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> Peter Meggik </td>
-                        <td>
-                          <ProgressBar variant="primary" now={50} />
-                        </td>
-                        <td> $ 77.99 </td>
-                        <td> May 15, 2015 </td>
-                      </tr>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face5.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> Edward </td>
-                        <td>
-                          <ProgressBar variant="danger" now={60} />
-                        </td>
-                        <td> $ 160.25 </td>
-                        <td> May 03, 2015 </td>
-                      </tr>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face6.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> John Doe </td>
-                        <td>
-                          <ProgressBar variant="info" now={65} />
-                        </td>
-                        <td> $ 123.21 </td>
-                        <td> April 05, 2015 </td>
-                      </tr>
-                      <tr>
-                        <td className="py-1">
-                          <img
-                            src={require("../../assets/images/faces/face7.jpg")}
-                            alt="user icon"
-                          />
-                        </td>
-                        <td> Henry Tom </td>
-                        <td>
-                          <ProgressBar variant="warning" now={20} />
-                        </td>
-                        <td> $ 150.00 </td>
-                        <td> June 16, 2015 </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
+            <Table type="FAILED" />
           </div>
           <div className="col-lg-12 grid-margin stretch-card">
             <div className="card">
